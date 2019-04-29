fbfcbee977de2d5dfa3d1d132d5cc45d2bc1c5df
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-29 20:39:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,16 +7,19 @@ def load_library(file)
   library = {}
   emojipedia = YAML.load_file(file)
   emojipedia.each do |meaning, emoticon|
-    library["get_meaning"] => {}
-    library["get_emoticon"] => {}
-  
+    library["get_meaning"] ||= {}
+    library["get_meaning"][emoticon[1]] = meaning 
+    
+    library["get_emoticon"] ||> {}
+    library["get_emoticon"[emoticon[0]] = emoticon[1]
+  end
   library 
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file, emoticon)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file,emoticon)
   # code goes here
 end
